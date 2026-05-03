74da84128cc465b231c61b0be25598f149a88050
Patrick Aziken <onaziken@gmail.com>
2026-05-03 21:30:26 +0100

    refactor: remove unused ArgumentCaptor import in CommentServiceTest for cleaner code

--- audita-api/infrastructure/src/test/java/io/audita/infrastructure/service/CommentServiceTest.java
+++ audita-api/infrastructure/src/test/java/io/audita/infrastructure/service/CommentServiceTest.java
@@ -10,7 +10,6 @@ import io.audita.infrastructure.persistence.repository.CommentRepository;
 import io.audita.infrastructure.persistence.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
